a199af42ce9ec13e2ab69154a6259c520ceedd11
sebastiankutschbach <sebastian.kutschbach@freenet.de>
2019-08-27 16:44:22 +0000

    forgot to save

--- src/LandroidSRouter.ts
+++ src/LandroidSRouter.ts
@@ -42,11 +42,7 @@ class LandroidSRouter extends BaseRouter {
     private setRainDelay(req: Request, res: Response, next: NextFunction): void {
         let value = req.params.value;
         try {
-<<<<<<< HEAD
             LandroidS.getInstance().setRainDelay(+value);
-=======
-            LandroidS.getInstance().setRainDelay(parseInt(value, 10));
->>>>>>> 4bca4b3887f3f32cab270a501114540a36481cc5
             this.ok(res);
         } catch (e) {
             this.badRequest(res, e.message);
@@ -56,11 +52,7 @@ class LandroidSRouter extends BaseRouter {
     private setTimeExtension(req: Request, res: Response, next: NextFunction): void {
         let value = req.params.value;
         try {
-<<<<<<< HEAD
             LandroidS.getInstance().setTimeExtension(+value);
-=======
-            LandroidS.getInstance().setTimeExtension(parseInt(value, 10));
->>>>>>> 4bca4b3887f3f32cab270a501114540a36481cc5
             this.ok(res);
         } catch (e) {
             this.badRequest(res, e.message);
@@ -71,11 +63,7 @@ class LandroidSRouter extends BaseRouter {
         let weekday = req.params.weekday;
         let payload = req.body;
         try {
-<<<<<<< HEAD
             LandroidS.getInstance().setSchedule(+weekday, payload);
-=======
-            LandroidS.getInstance().setSchedule(parseInt(weekday, 10), payload);
->>>>>>> 4bca4b3887f3f32cab270a501114540a36481cc5
             this.ok(res);
         } catch (e) {
             this.badRequest(res, e.message);
